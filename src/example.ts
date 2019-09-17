decbe1ab8df384e0504d834679738cafee024021
Chanlito <chanlitohem@gmail.com>
2019-09-17 15:55:29 +0700

    feat: add example

---
+++ src/example.ts
@@ -0,0 +1,103 @@
+import gql from 'graphql-tag';
+import { Component, Vue } from 'vue-property-decorator';
+
+import { SmartQuery } from '../src';
+
+@Component
+export class TodoList extends Vue {
+  @SmartQuery({
+    query: gql`
+      query Todos($skip: Int, $limit: Int) {
+        todos(skip: $skip, limit: $limit) {
+          id
+          title
+        }
+      }
+    `,
+    variables() {
+      return this.vars;
+    }
+  })
+  todos!: Todo[];
+
+  get vars(): QueryVariables {
+    return {
+      limit: 10,
+      skip: 0
+    };
+  }
+}
+
+@Component
+export class TodoList1 extends TodoList {
+  @SmartQuery<TodoList1>({
+    query: gql`
+      query Todos($skip: Int, $limit: Int) {
+        todos(skip: $skip, limit: $limit) {
+          id
+          title
+        }
+      }
+    `,
+    variables() {
+      return this.vars;
+    }
+  })
+  todos!: Todo[];
+}
+
+@Component
+export class TodoList2 extends TodoList {
+  @SmartQuery<TodoList2, QueryResult>({
+    query: gql`
+      query Todos($skip: Int, $limit: Int) {
+        todos(skip: $skip, limit: $limit) {
+          id
+          title
+        }
+      }
+    `,
+    variables() {
+      return this.vars;
+    },
+    update(data) {
+      // data: QueryResult
+    }
+  })
+  todos!: Todo[];
+}
+
+@Component
+export class TodoList3 extends TodoList {
+  @SmartQuery<TodoList3, QueryResult, QueryVariables>({
+    query: gql`
+      query Todos($skip: Int, $limit: Int) {
+        todos(skip: $skip, limit: $limit) {
+          id
+          title
+        }
+      }
+    `,
+    variables() {
+      return this.vars;
+    },
+    update(data) {
+      // data: QueryResult
+    }
+  })
+  todos!: Todo[];
+}
+
+interface Todo {
+  id: string;
+  title: string;
+}
+
+interface QueryResult {
+  todos: Todo[];
+}
+
+interface QueryVariables {
+  skip?: number;
+  limit?: number;
+}
